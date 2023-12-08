22c0eccc747edd1d2252a0f7ad8cce8ed4f735a5
Tommy Johnsen <7oiden@gmail.com>
2023-12-08 15:22:14 +0100

    design  update

--- js/ui/renderMovies.js
+++ js/ui/renderMovies.js
@@ -8,16 +8,24 @@ export function renderMovies(movies) {
   movies.forEach((movie) => {
     moviesContainer.innerHTML += `
         <div class="movie__container">
+        <div class="movie__poster">
             <img src="${movie.Poster}" alt="${movie.Title}">
+        </div>
             <div class="movie__card">
                 <div class="movie__header">
                     <h2>${movie.Title} (${movie.Year})</h2>
+                    <div class="movie__rating">
+                    <i class="fas fa-star"></i>
                     <p>${movie.imdbRating}</p>
+                    </div>
                 </div>
                 <div class="movie__info">
                     <p>${movie.Runtime}</p>
                     <p>${movie.Genre}</p>
+                    <div class="movie__watchlist">
+                    <i class="fas fa-plus-circle"></i>
                 <p>Watchlist</p>
+                </div>
                 </div>
                 <div class="movie__plot">
                 <p>${movie.Plot}</p>
